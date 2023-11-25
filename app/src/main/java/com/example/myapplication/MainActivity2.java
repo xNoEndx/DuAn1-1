b6c48bb59831cb9f6a033485ffe6f73ebb93aae7
Phong <you@example.com>
2023-11-25 19:02:59 +0700

    update code 25/11

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import com.google.android.material.tabs.TabLayout;
+public class MainActivity2 extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wellcome);
+        setContentView(R.layout.activity_main);
     }
 }
